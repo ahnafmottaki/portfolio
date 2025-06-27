6a71d620dd5fc3244494ce19261a9f892bd80ed7
FreeCodeJunction <ahnafmottaki2022@gmail.com>
2025-06-28 01:52:18 +0600

    set up project and created responsive navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section className="" id="home">
+      <div className="">
+        <div>
+          <div className="">
+            <figure className="">
+              <img src="" alt="" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
