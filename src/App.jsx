b93bb9dddf234e1312e0808330b61a172bb5e7a7
FreeCodeJunction <ahnafmottaki2022@gmail.com>
2025-06-28 09:05:29 +0600

    create skill section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skill from "./components/Skill";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <main>
         <Hero />
         <About />
+        <Skill />
       </main>
     </>
   );
